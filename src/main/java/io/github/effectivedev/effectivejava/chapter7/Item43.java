06677c4c74c266af0c6847c538173598a25c9094
Inyoung <in0@me.com>
2019-10-21 15:55:45 +0900

    second commit

---
+++ src/main/java/io/github/effectivedev/effectivejava/chapter7/Item43.java
@@ -0,0 +1,11 @@
+package io.github.effectivedev.effectivejava.chapter7;
+
+/**
+ * effectivejava
+ * Item43
+ * Github : https://github.com/effectivedev
+ * Created by in0@me.com on 2019/10/21 3:55 PM
+ */
+
+public class Item43 {
+}
