270457e0c912c11da6066783eb9651f22edcc9de
Inyoung <in0@me.com>
2019-10-21 17:19:41 +0900

    item 46 add

--- src/main/java/io/github/effectivedev/effectivejava/chapter7/Item43.java
+++ src/main/java/io/github/effectivedev/effectivejava/chapter7/Item43.java
@@ -1,11 +1,34 @@
 package io.github.effectivedev.effectivejava.chapter7;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * effectivejava
- * Item43
+ * Item43 :  람다보다는 메서드 참조를 사용하라
  * Github : https://github.com/effectivedev
  * Created by in0@me.com on 2019/10/21 3:55 PM
  */
 
 public class Item43 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("1234");
+        int[] arr;
+
+        //lambda
+        arr = list.stream().mapToInt(str -> Integer.parseInt(str)).toArray();
+        //Integer::parseInt
+        arr = list.stream().mapToInt(Integer::parseInt).toArray();
+
+
+        list = new ArrayList<String>();
+        list.add("abBcdde");
+        // str -> str.toLowerCase()
+        list.stream().map(str -> str.toLowerCase()).collect(Collectors.toList()).forEach(System.out::println);
+        //String::toLowerCase
+        list.stream().map(String::toLowerCase).collect(Collectors.toList()).forEach(System.out::println);
+
+    }
 }
---
+++ src/main/java/io/github/effectivedev/effectivejava/chapter7/Item46.java
@@ -0,0 +1,38 @@
+package io.github.effectivedev.effectivejava.chapter7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+/**
+ * effectivejava
+ * Item46
+ * Github : https://github.com/effectivedev
+ * Created by in0@me.com on 2019/10/21 5:08 PM
+ */
+
+public class Item46 {
+    public static void main(String[] args) {
+        Map<String, Long> freq;
+        List<String> list = new ArrayList<>();
+        list.add("ABCD");
+        list.add("abCd");
+        list.add("abcD");
+        list.add("abcde");
+        list.add("abcd");
+        list.add("bcd");
+        list.add("bcd");
+        list.add("test");
+        freq = list.stream().collect(groupingBy(String::toLowerCase, counting()));
+        System.out.println(freq);
+
+        List<String> topTen = freq.keySet().stream().sorted(comparing(freq::get).reversed())
+                .limit(2).collect(Collectors.toList());
+        System.out.println(topTen);
+    }
+}
